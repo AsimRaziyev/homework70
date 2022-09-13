fb862dd241913fd3d10e969ba4bebb000cb2278f
Assym <asimo_555@mail.ru>
2022-09-14 03:08:47 +0600

    commit

---
+++ source/webapp/views/articles.py
@@ -0,0 +1,146 @@
+from django.shortcuts import render
+
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.db.models import Q
+from django.urls import reverse_lazy
+
+# Create your views here.
+from django.utils.http import urlencode
+from django.views import View
+
+from webapp.forms import ArticleForm, SearchForm, ArticleDeleteForm, UserArticleForm
+from webapp.models import Article
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+
+
+class AddLike(LoginRequiredMixin, View):
+
+    def get(self, request, pk, *args, **kwargs):
+        article = Article.objects.get(pk=pk)
+        print(article)
+        if article.likes.filter(id=request.user.id).exists():
+            article.likes.remove(request.user)
+            article.like_count -= 1
+            result = article.like_count
+            print(result)
+            article.save()
+        else:
+            article.likes.add(request.user)
+            article.like_count += 1
+            result = article.like_count
+            article.save()
+
+        return JsonResponse({'result': result})
+
+
+# def post(self, request, pk, *args, **kwargs):
+#     article = Article.objects.get(pk=pk)
+#     print(article)
+#     is_like = False
+#     for like in article.likes.all():
+#         if like == request.user:
+#             is_like = True
+#             break
+#     if not is_like:
+#         article.likes.add(request.user)
+#         print(article)
+#     if is_like:
+#         article.likes.remove(request.user)
+#     return HttpResponseRedirect(reverse('article', args=[str(pk)]))
+
+# context = {"article": article}
+# return render(request, 'webapp:index.html', context)
+
+
+class IndexView(ListView):
+    model = Article
+    template_name = "articles/index.html"
+    context_object_name = "articles"
+    ordering = "-updated_at"
+    paginate_by = 6
+
+    def get(self, request, *args, **kwargs):
+        self.form = self.get_search_form()
+        self.search_value = self.get_search_value()
+        return super().get(request, *args, **kwargs)
+
+    def get_queryset(self):
+        if self.search_value:
+            return Article.objects.filter(
+                Q(author__icontains=self.search_value) |
+                Q(title__icontains=self.search_value)).order_by("-updated_at")
+        return Article.objects.all().order_by("-updated_at")
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=object_list, **kwargs)
+        context["form"] = self.form
+        if self.search_value:
+            query = urlencode({'search': self.search_value})  # search=dcsdvsdvsd
+            context['query'] = query
+            context['search'] = self.search_value
+        return context
+
+    def get_search_form(self):
+        return SearchForm(self.request.GET)
+
+    def get_search_value(self):
+        if self.form.is_valid():
+            return self.form.cleaned_data.get("search")
+
+
+class ArticleView(DetailView):
+    template_name = "articles/article_view.html"
+    model = Article
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comments'] = self.object.comments.order_by("-created_at")
+        return context
+
+
+class CreateArticle(LoginRequiredMixin, CreateView):
+    form_class = ArticleForm
+    template_name = "articles/create.html"
+
+    # def dispatch(self, request, *args, **kwargs):
+    #     if request.user.is_authenticated and self.request.user.has_perm("webapp.add_article"):
+    #         return super().dispatch(request, *args, **kwargs)
+    #     return redirect("accounts:login")
+
+    def form_valid(self, form):
+        user = self.request.user
+        form.instance.author = user
+        return super().form_valid(form)
+
+
+class UpdateArticle(PermissionRequiredMixin, UpdateView):
+    form_class = ArticleForm
+    template_name = "articles/update.html"
+    model = Article
+
+    def has_permission(self):
+        return self.request.user.has_perm("webapp.change_article") or \
+               self.request.user == self.get_object().author
+
+
+class DeleteArticle(PermissionRequiredMixin, DeleteView):
+    model = Article
+    template_name = "articles/delete.html"
+    success_url = reverse_lazy('webapp:index')
+    form_class = ArticleDeleteForm
+    permission_required = "webapp.delete_article"
+
+    def has_permission(self):
+        return super().has_permission() or self.request.user == self.get_object().author
+
+        # return self.request.user.is_superuser or \
+        #        self.request.user.groups.filter(name__in=("Модераторы",)).exists()
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(data=request.POST, instance=self.get_object())
+        if form.is_valid():
+            return self.delete(request, *args, **kwargs)
+        else:
+            return self.get(request, *args, **kwargs)
